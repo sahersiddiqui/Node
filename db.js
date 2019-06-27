1c54fe292332caf7a4884ba08346bf4b7a9308ec
saher <najmussaher.siddiqui@appinventiv.com>
2019-06-27 15:19:57 +0530

    changes

---
+++ db.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
+
+connection.connect()
+
+// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
+
+connection.end()
